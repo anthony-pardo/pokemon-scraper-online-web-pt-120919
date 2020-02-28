e829bd6a63484be92335e51e6c9f4cd4ad79d115
Anthony Pardo <pardo.anthony@gmail.com>
2020-02-28 17:58:10 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,10 +2,13 @@ class Pokemon
   attr_accessor :name, :type
   attr_reader :id, :db 
   
+  @@all = []
+  
   def initialize(id=nil, name, type, db)
     @id = id 
     @name = name 
     @type = type 
     @db = db 
+    @@all << self
   end
 end
